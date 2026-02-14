264fd0ef9ef28c17ade37e2fecc3e0e00e293f34
Brandon Auyeung <bauyeung@tuta.com>
2026-02-13 17:29:26 -0800

    much is wrong with the clay settlement

--- bAuyeung_Settlement_CE444.py
+++ bAuyeung_Settlement_CE444.py
@@ -1,4 +1,5 @@
 from math import log10
+import numpy as np
 
 class Foundation:
     def __init__(self, B, L, D, H, surcharge = 'Na',thickness = 'Na', point_load = 'Na', gamma = 'Na', metric = True):
@@ -26,10 +27,24 @@ class Foundation:
     def influence_depth(self):
         self.influence_depth = self.refL * ((self.B / self.refL)**0.79)
 
-    def soil(self, gamma_soil, gwt, avgN60, excavation = False):
+    def sand(self, gamma_soil, gwt, avgN60, excavation = False):
         self.gamma_soil = gamma_soil # match units specified by the foundation
         self.gwt = gwt
         self.avgN60 = avgN60
+        if excavation == False:
+            self.excavation = 'No'
+        else:
+            self.excavation = excavation # excavation must be in specified units
+
+    def clay(self, gamma_soil, gwt, Su, PI, K, e0, OCR = 1, excavation = False):
+        self.gamma_soil = gamma_soil # match units specified by the foundation
+        self.gwt = gwt
+        self.Su = Su
+        self.PI = PI
+        self.K = K # obtain K from the Duncan and Buchignani (1987) graph
+        self.Eu = K * Su
+        self.OCR_clay = OCR
+        self.e0 = e0
         if excavation == False:
             self.excavation = 'No'
         else:
@@ -77,8 +92,55 @@ class Foundation:
             print('NC Settlement Case')
             self.settlement = (self.refL )* (0.1*self.shape_factor*self.layer_thickness_factor*self.time_factor*self.comp_index*((self.B/self.refL)**0.7)) * (self.load / self.Pa)
 
-    def settlement_clay(self, time, depth_to_base, I0 = -1, I1 = -1, OCR = 1):
-        # 
+    def settlement_clay(self, points: np.array, Cc = 'Na', Cr = 'Na', layer_thickness = 'Na', net = True, I0 = 'Na', I1 = 'Na'):
+
+        if self.gwt == 0:
+            self.excav_load = (self.gamma_soil - self.gamma_w)*self.excavation
+        elif 0 < self.gwt < self.excavation:
+            self.excav_load = (self.gamma_soil * self.excavation) - ((self.gamma_w) * ( self.excavation - self.gwt))
+        else:
+            self.excav_load = (self.gamma_soil * self.excavation)
+        
+        if net == True:
+            self.qb = self.load - self.excav_load
+        else:
+            self.qb = self.load + self.excav_load
+
+        self.veoStress_clay = np.zeros(len(points))
+
+        for index, value in enumerate(points):
+            if self.gwt == 0:
+                self.veoStress_clay[index] = (self.gamma_soil - self.gamma_w)*value
+            elif 0 < self.gwt < value:
+                self.veoStress_clay[index] = (self.gamma_soil * value) - ((self.gamma_w) * ( value - self.gwt))
+            else:
+                self.veoStress_clay[index] = (self.gamma_soil * value)
+        
+        self.w_i = I0 * I1  * ((self.load * self.B)/self.Eu) #immediate settlement in clay
+        
+        self.vefStress_clay = self.veoStress_clay + ((self.load * self.B * self.L)/((self.B + (points - self.D))*(self.L + (points - self.D))))
+
+        self.preconsol_stress = self.OCR_clay * self.veoStress_clay
+
+        self.w_c = np.zeros(len(self.preconsol_stress))
+        for index, value in enumerate(self.preconsol_stress):
+            if self.OCR_clay == 1 or self.veoStress_clay[index] == self.preconsol_stress[index]:   
+                print('NC Settlement Case')
+                self.w_c[index] = (layer_thickness/(1 + self.e0)) * Cc * np.log10(self.vefStress_clay[index]/self.veoStress_clay[index])
+            elif self.veoStress_clay[index] < self.vefStress_clay[index] < self.preconsol_stress[index]:
+                print('OCOC Settlement Case')
+                self.w_c[index] = (layer_thickness/(1 + self.e0)) * Cr * np.log10(self.vefStress_clay[index]/self.veoStress_clay[index])
+            else:
+                print('OCNC Settlement Case')
+                self.w_c[index] = (layer_thickness/(1 + self.e0)) * (((Cr * (np.log10(self.preconsol_stress[index]/self.veoStress_clay[index]))) + ((Cc * (np.log10(self.vefStress_clay[index]/self.preconsol_stress[index]))))))
+        print('')
+        print('Immediate Settlement: ', self.w_i)
+        print('')
+        print('Settlement in Clay Layers: ', self.w_c)
+        print('')
+
+        self.settlement = np.sum(self.w_c) + self.w_i
+
 
     def display_foundation(self):
         print(f'Unit System: {self.unit_system}')
@@ -91,13 +153,24 @@ class Foundation:
         print(f'Unit Weight of Foundation: {self.gamma}')
         print('')
 
-    def display_soil(self):
+    def display_sand(self):
         print(f'Unit System: {self.unit_system}')
         print(f'Unit Weight of Soil: {self.gamma_soil}')
         print(f'Average N60: {self.avgN60}')
-        print(f'Unit Weight of Water: {self.gamma_w}')
         print(f'Groundwater table depth: {self.gwt}')
         print(f'Excavation is present to depth {self.excavation}')
+        print('')
+
+    def display_clay(self):
+        print(f'Unit System: {self.unit_system}')
+        print(f'Unit Weight of Soil: {self.gamma_soil}')
+        print(f'Groundwater table depth: {self.gwt}')
+        print(f'Excavation is present to depth {self.excavation}')
+        print(f'Su: {self.Su}')
+        print(f'PI: {self.PI}')
+        print(f'OCR: {self.OCR_clay}')
+        print(f'K: {self.K}')
+        print(f'e0: {self.e0}')
         print('')
 
     def display_factors(self): # display the results
